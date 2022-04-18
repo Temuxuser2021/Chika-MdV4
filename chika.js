9f9633b44acb69cc77a93b76b99824132d5f6937
NOOB hacker raj <87321079+Temuxuser2021@users.noreply.github.com>
2022-04-18 12:53:52 +0530

    Update chika.js

--- chika.js
+++ chika.js
@@ -1,7 +1,7 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
-   * Follow https://github.com/DikaArdnt
+   * Create By Noob Programer official.
+   * Contact Me on wa.me/919707905478
+   * Follow https://github.com/Temuxuser2021
 */
 
 require('./settings')
